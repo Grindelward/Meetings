30ea15da17bc8a887a0aa37f2e251167ed520c1e
Grindelward <przemyslaw.nowicki93@gmail.com>
2017-05-15 21:15:29 +0200

    lets the party begin

---
+++ www/js/pgb.js
@@ -0,0 +1,39 @@
+function init() {
+	document.addEventListener("deviceready",onDeviceReady, false);
+}
+
+function onDeviceReady() {
+	navigator.notification.beep(2);
+	deviceInfo();
+}
+
+function deviceInfo() {
+
+	info =  'Device Model   : '    + device.model + '<br>' + 
+			'Device Name    : '     + device.name + '<br>' + 
+			'Device Cordova : '  + device.cordova + '<br>' + 
+			'Device Platform: ' + device.platform + '<br>' + 
+			'Device UUID    : '     + device.uuid + '<br>' + 
+			'Device Version : '  + device.version + '<br>';
+
+	document.getElementById("deviceDetails").innerHTML = info;	
+}
+/*
+$( document ).on( "pagecreate", "#demo-page", function() {
+    $( document ).on( "swipeleft swiperight", "#demo-page", function( e ) {
+        // We check if there is no open panel on the page because otherwise
+        // a swipe to close the left panel would also open the right panel (and v.v.).
+        // We do this by checking the data that the framework stores on the page element (panel: open).
+        if ( $( ".ui-page-active" ).jqmData( "panel" ) !== "open" ) {
+            if ( e.type === "swipeleft" ) {
+                $( "#right-panel" ).panel( "open" );
+            }
+        }
+    });
+});
+*/
+function testChange() {
+
+	 $('#content').html( $('#source-content').html() ); 
+	 
+}
