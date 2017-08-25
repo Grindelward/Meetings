938daeea2fa1140de845d17743abc0e349022f12
Grindelward <przemyslaw.nowicki93@gmail.com>
2017-08-25 16:55:47 +0200

    calendard

--- src/pages/create-meeting/create-meeting.ts
+++ src/pages/create-meeting/create-meeting.ts
@@ -3,6 +3,10 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Meeting } from "../../models/meeting";
 import { AngularFireDatabase, FirebaseListObservable } from "angularfire2/database";
 import { AngularFireAuth } from "angularfire2/auth";
+import { Calendar } from '@ionic-native/calendar';
+
+
+
 /**
  * Generated class for the CreateMeetingPage page.
  *
@@ -22,11 +26,15 @@ export class CreateMeetingPage {
   users: FirebaseListObservable<any[]>;
   checked = {members:[]};
 
-  constructor(public afAuth: AngularFireAuth, public navCtrl: NavController, public navParams: NavParams, public  angFire: AngularFireDatabase) {
+  constructor(public afAuth: AngularFireAuth, public navCtrl: NavController, public navParams: NavParams, public  angFire: AngularFireDatabase, private calendar: Calendar) {
     this.meeting.members = []; // place for members must be defined as array at begins
     this.meeting.organizator = this.afAuth.auth.currentUser.email //organizator is currentUser, needed for validation member list organizator is always member
    
     this.users = angFire.list('/Users'); //list for members you could invite
+    this.calendar.createCalendar('MyCalendar').then(
+      (msg) => { console.log(msg); },
+      (err) => { console.log(err); }
+    );
   }
 
   ionViewDidLoad() {
@@ -36,6 +44,9 @@ export class CreateMeetingPage {
   confirmMeeting(meeting: Meeting){
     this.meetings = this.angFire.list('/Meetings'); 
     this.meetings.push(meeting) // pushing into firebase database
+
+  this.calendar.createEvent(meeting.topic, meeting.address, meeting.description, new Date(), new Date())
+  this.calendar.openCalendar(meeting.date)
 
   }
 
